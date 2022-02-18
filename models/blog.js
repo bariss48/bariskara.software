af3bdf53fa5217a0c24c1fcd49f94c933395b403
bariss48 <berkk.baris48@gmail.com>
2022-02-18 13:46:05 +0300

    initial commit

---
+++ models/blog.js
@@ -0,0 +1,34 @@
+const { text } = require("body-parser");
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+     title:String,
+     intro:String,
+     description: String,
+     description_2: String,
+     code: String,
+     code_2: String,
+     read_min: String,
+     author: String,
+     github_link: String,
+     pdf_link: String,
+     date: Date,
+     genre: String,
+     image_link: String,
+     image_link_2: String,     
+     owner:{
+          id:{
+             type: mongoose.Schema.Types.ObjectId,
+             ref: "User" 
+          },
+          username: String
+     },
+     upvotes: [String],
+     downvotes: [String]
+});
+
+blogSchema.index({
+     '$**': 'text'
+});
+
+module.exports = mongoose.model("blog",blogSchema);
